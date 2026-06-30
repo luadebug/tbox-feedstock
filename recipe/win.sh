bbd9805ed3498e656e765636aa4818964d99f483
Saikari <lin@sz.cn.eu.org>
2026-06-30 19:42:33 +0300

    fixup

--- recipe/win.sh
+++ recipe/win.sh
@@ -43,4 +43,4 @@ install -Dm644 "${BUILD_DIR}/tbox.lib" "${PREFIX}/lib/tbox.lib"
 mkdir -p "${PREFIX}/include"
 cp -r src/tbox "${PREFIX}/include/"
 
-install -Dm644 "${BUILD_DIR}/tbox.config.h" "${PREFIX}/Library/include/tbox/tbox.config.h"
+install -Dm644 "${BUILD_DIR}/tbox.config.h" "${PREFIX}/include/tbox/tbox.config.h"
